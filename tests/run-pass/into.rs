da3683228472e8be9f4a2ec6228f3baca707b279
Pierre Chifflier <chifflier@wzdftpd.net>
2021-05-06 13:58:18 +0200

    Add test file for Into

---
+++ tests/run-pass/into.rs
@@ -0,0 +1,33 @@
+use nom::character::streaming::alpha1;
+use nom::IResult;
+use nom_derive::*;
+
+fn parser1(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    alpha1(i)
+}
+
+#[derive(Debug, PartialEq, NomBE)]
+// #[nom(DebugDerive)]
+struct IntoStruct {
+    #[nom(Into, Parse = "parser1")]
+    a: Vec<u8>,
+    b: u16,
+    c: u16,
+}
+
+fn main() {
+    let input = b"abcd\x00\x01\x00\x02";
+
+    let res = IntoStruct::parse(input);
+    assert_eq!(
+        res,
+        Ok((
+            &input[8..],
+            IntoStruct {
+                a: vec![97, 98, 99, 100],
+                b: 1,
+                c: 2,
+            }
+        ))
+    )
+}
